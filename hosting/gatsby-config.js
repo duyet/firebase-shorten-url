a6fab519ec600b4d3cf77651cb9ff47ce1637c98
Duyet Le <lvduyet@fossil.com>
2019-09-18 23:48:54 +0700

    Update deployment

--- hosting/gatsby-config.js
+++ hosting/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `duyet shorten url`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Shorten URL by Firebase Dynamic Link`,
+    author: `@duyetdev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,9 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: ['UA-92451506-6'],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
   ],
   proxy: {
     prefix: '/api',
