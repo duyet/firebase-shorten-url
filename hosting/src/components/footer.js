bd18fe3efe0732f620e3940b92911ea8f358d383
Duyet Le <5009534+duyet@users.noreply.github.com>
2022-02-05 06:06:49 +0000

    chore: footer

--- hosting/src/components/footer.js
+++ hosting/src/components/footer.js
@@ -7,10 +7,11 @@ const FooterContaner = () => (
         <Footer style={{ position: 'absolute', bottom: 0, width: '100%' }}>
             © {new Date().getFullYear()}, Built with
             {' '}
-
             <a href="https://www.gatsbyjs.org">Gatsby</a>
             {', '}
             <a href="https://firebase.google.com">Firebase</a>
+            {' - '}
+            <a href="https://github.com/duyet/firebase-shorten-url">Open Source</a>
         </Footer>
 );
 
